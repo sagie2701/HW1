9cc7084ab51dd948a3fbfb44bd70c51919b45b8c
amit <tavor7@gmail.com>
2023-05-11 16:26:48 +0300

    added comments in board and tile - need to continue

--- src/Tile.java
+++ src/Tile.java
@@ -5,7 +5,12 @@ public class Tile {
     private int column;
     private int[] manhettenDistance = new int[2];
 
-
+    /**
+     *
+     * @param tileNum
+     * @param row
+     * @param column
+     */
     public Tile(String tileNum, int row, int column){
         if (tileNum.equals("_"))
             this.value = 0;
@@ -15,32 +20,60 @@ public class Tile {
         this.column = column;
     }
 
+    /**
+     *
+     * @param tile
+     */
     public Tile(Tile tile){
         this.value = tile.getValue();
         this.row = tile.getRow();
         this.column = tile.getColumn();
     }
 
+    /**
+     * returns the value of the tile
+     * @return the value of the tile
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * returns the row the tile is on
+     * @return the row the tile is on
+     */
     public int getRow() {
         return row;
     }
 
+    /**
+     * change the row the tile is on to a given row
+     * @param row - the new row of the tile
+     */
     public void setRow(int row) {
         this.row = row;
     }
 
+    /**
+     * returns the row the tile is on
+     * @return the row the tile is on
+     */
     public int getColumn() {
         return column;
     }
 
+    /**
+     * change the column the tile is on to a given column
+     * @param column - the new column of the tile
+     */
     public void setColumn(int column) {
         this.column = column;
     }
 
+    /**
+     * switch between two tiles
+     * @param tile - a tile object to switch the current one with
+     */
     public void switchPlace(Tile tile){
         int tempRow = this.row, tempColumn = this.column;
         this.row = tile.getRow();
@@ -49,16 +82,29 @@ public class Tile {
         tile.setColumn(tempColumn);
     }
 
+    /**
+     * move tile to new location by row and column
+     * @param row - new row location for tile
+     * @param column - new column location for tile
+     */
     public void switchPlaceByPlace(int row, int column){
         int tempRow = this.row, tempColumn = this.column;
         this.row = row;
         this.column = column;
     }
 
+    /**
+     * returns the Manhetten Distance of the tile
+     * @return the Manhetten Distance of the tile
+     */
     public int[] getManhettenDistance() {
         return manhettenDistance;
     }
 
+    /**
+     * calculate the Manhetten Distance of the tile
+     * @param columns - the number of columns in the board
+     */
     public void manhettenDistance(int columns){
         this.manhettenDistance[0] = this.value / columns;
         this.manhettenDistance[1] = (this.value % columns) - 1;
@@ -74,17 +120,21 @@ public class Tile {
             this.manhettenDistance[1] *= -1;
     }
 
+    /**
+     * set the Manhetten Distance to a given value
+     * @param manhettenDistance - array that contain the Manhetten Distance, [0]-moves in x, [1]-moves in y
+     */
     public void setManhettenDistance(int[] manhettenDistance){
         this.manhettenDistance[0] = manhettenDistance[0];
         this.manhettenDistance[1] = manhettenDistance[1];
     }
 
+    /**
+     * returns the Manhetten Distance
+     * @return the Manhetten Distance
+     */
     public int getManhettenDistanceSum(){
         return this.manhettenDistance[0] + this.manhettenDistance[1];
-    }
-
-    public boolean isInTarget(){
-        return this.manhettenDistance[0] == 0 && this.manhettenDistance[1] == 0;
     }
 
     @Override
